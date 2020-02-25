e15a1108cf8f093d1a6dd617c6f9163aea1d772d
Hawk Nguyen <verynoob189@gmail.com>
2020-02-25 00:24:05 -0500

    start integrate adminlte template into react

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Mainpage from "./Components/Mainpage/Mainpage";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App wrapper">
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+      <Mainpage></Mainpage>
+      <Footer></Footer>
     </div>
   );
 }
