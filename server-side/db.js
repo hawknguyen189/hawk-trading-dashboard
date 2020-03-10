e6ae6b5998a52202edaa7ece691c2d4cf31c120a
Hawk Nguyen <verynoob189@gmail.com>
2020-03-10 00:53:05 -0400

    add mongodb

---
+++ server-side/db.js
@@ -0,0 +1,37 @@
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://verynoob189:<password>@cluster0-m435n.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+// var mongoose = require("mongoose");
+
+// mongoose.connect(
+//   "mongodb+srv://username:password@cluster0-jyznc.mongodb.net/test?retryWrites=true&w=majority",
+//   { useNewUrlParser: true }
+// );
+
+// mongoose.connection.on("connected", function() {
+//   console.log("Mongoose default connection open");
+// });
+
+// mongoose.connection.on("error", function(err) {
+//   console.log("Mongoose default connection error: " + err);
+// });
+
+// mongoose.connection.on("disconnected", function() {
+//   console.log("Mongoose default connection disconnected");
+// });
+
+// process.on("SIGINT", function() {
+//   mongoose.connection.close(function() {
+//     console.log(
+//       "Mongoose default connection disconnected through app termination"
+//     );
+//     process.exit(0);
+//   });
+// });
