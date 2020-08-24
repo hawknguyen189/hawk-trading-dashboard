d6fa212c263087c53535a800a12ec8a77eb8a968
hawknguyen189 <verynoob189@gmail.com>
2020-08-23 22:27:56 -0400

    update side bar

--- client-side/src/Components/Sidebar/Sidebar.js
+++ client-side/src/Components/Sidebar/Sidebar.js
@@ -21,14 +21,14 @@ const Sidebar = () => {
         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
           <div className="image">
             <img
-              src={require("../../dist/img/user2-160x160.jpg")}
+              src={require("../../dist/img/admin.jpg")}
               className="img-circle elevation-2"
               alt="User Image"
             />
           </div>
           <div className="info">
             <a href="#" className="d-block">
-              Alexander Pierce
+              Kiwi The Boss
             </a>
           </div>
         </div>
